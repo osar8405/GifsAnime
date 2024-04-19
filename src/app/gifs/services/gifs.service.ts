8beaf902f0cd44acf8e9590242986360e2653636
Oscar Mireles Garcia <osar8405@gmail.com>
2024-04-18 23:00:36 -0600

    Consumiendo api gif

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,16 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gif.interfaces';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
 
+    public gifList: Gif[] = [];
     private _tagsHistory: string[] = [];
+    private apiKey: string = 'xJoNmYikoChaQ79uvSsGa8Yi0NyHy6Ex';
+    private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-    constructor() { }
+    constructor(private http: HttpClient) { }
 
     get tagsHistory() {
         return [...this._tagsHistory];
@@ -23,5 +28,14 @@ export class GifsService {
     searchTag(tag: string): void {
         if (tag.length === 0) return;
         this.organizeHistory(tag);
+        const params = new HttpParams()
+        .set('api_key', this.apiKey)
+        .set('limit', '10')
+        .set('q', tag)
+        this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params})
+        .subscribe(resp => {
+            this.gifList = resp.data;
+            console.log("Resp: ", {gifs: this.gifList});
+        })
     }
 }
